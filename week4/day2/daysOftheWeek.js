6b95fef67ebad666acd7a85a3304c7eba4930e89
sahin-x1 <sahinyes@proton.me>
2023-04-18 09:45:39 +0200

    day4

---
+++ week4/day2/daysOftheWeek.js
@@ -0,0 +1,58 @@
+var inputDay = "Sunday"
+
+// if (inputDay === "Monday")
+// {
+//     console.log("Montag")
+// } else if (inputDay === "Tuesday")
+// {
+//     console.log("Dienstag")
+// } else if (inputDay === "Wednesday")
+// {
+//     console.log("Mittwoch")
+// } else if (inputDay === "Thursday")
+// {
+//     console.log("Donnerstag")
+// } else if (inputDay === "Friday")
+// {
+//     console.log("Freitag")
+// } else if (inputDay === "Saturday")
+// {
+//     console.log("Samstag")
+// } else if ( inputDay === "Sunday")
+// {
+//     console.log("Sonntag")
+// } else {
+//     console.log("Input must be like example: 'Monday'")
+// }
+
+
+switch (inputDay)
+{
+    case "Monday":
+        console.log("Montag")
+        break
+
+    case "Tuesday":
+        console.log("Dienstag")
+        break
+
+    case "Wednesday":
+        console.log("Mittwoch")
+        break
+
+    case "Thursday":
+        console.log("Donnerstag")
+        break
+
+    case "Friday":
+        console.log("Freitag")
+        break
+
+    case "Saturday":
+        console.log("Samstag")
+        break
+
+    case "Sunday":
+        console.log("Sontag")
+        break
+}
